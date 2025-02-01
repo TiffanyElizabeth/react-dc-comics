36136f8bc6319ee882736cfaac0fdf0aadaf6556
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2025-02-01 14:28:04 +0100

    feat: add footer-top

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,50 @@
 export default function Footer() {
-    return <footer>Footer</footer>
+    return (
+        <footer>
+            <section className="footer-top">
+                <div className="container">
+                    <nav className="footer-top-nav">
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    <img className="footer-top-nav-img" src="../img/buy-comics-digital-comics.png" alt="buy digital comics"></img>
+                                    <p>digital comics</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img className="footer-top-nav-img" src="../img/buy-comics-merchandise.png" alt="buy merchandise"></img>
+                                    <p>dc merchandise</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img className="footer-top-nav-img" src="../img/buy-comics-subscriptions.png" alt="subscribe"></img>
+                                    <p>subscription</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img className="footer-top-nav-img" src="../img/buy-comics-shop-locator.png" alt="shop locator"></img>
+                                    <p>comic shop locator</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img className="footer-top-nav-img" src="../img/buy-comics-digital-comics.png" alt="shop locator"></img>
+                                    <p>dc power visa</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </section>
+            <section className="footer-mid">
+                <div className="container">hello</div>
+            </section>
+            <section className="footer-bottom">
+                <div className="container">hello</div>
+            </section>
+        </footer>
+    )
 }
